62069bbd87bf6375fd5bd891b5b380e110b43ee5
BreadFish64 <mohror64@gmail.com>
2018-06-10 15:20:06 -0500

    remove leftover switch

--- pot8o-chip/chip8.cpp
+++ pot8o-chip/chip8.cpp
@@ -54,203 +54,7 @@ void Chip8::emulate() {
 
 void Chip8::emulateCycle() {
     opcode = memory[pc] << 8 | memory[pc + 1];
-    CPU::opcode_table[op()](*cpu.get());
-
-    /*switch (opcode & 0xF000) {
-    case 0x0000:
-        switch (opcode & 0x00FF) {
-        case 0x00E0:
-            std::fill(gfx.begin(), gfx.end(), 0);
-            break;
-
-        case 0x00EE:
-            pc = stack[sp];
-            sp--;
-            break;
-
-        default:
-            printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
-        }
-        break;
-
-    case 0x1000:
-        pc = (opcode & 0x0FFF) - 2;
-        break;
-
-    case 0x2000:
-        sp++;
-        stack[sp] = pc;
-        pc = (opcode & 0x0FFF) - 2;
-        break;
-
-    case 0x3000:
-        if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
-            pc += 2;
-        break;
-
-    case 0x4000:
-        if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
-            pc += 2;
-        break;
-
-    case 0x6000:
-        V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF);
-        break;
-
-    case 0x7000:
-        V[(opcode & 0x0F00) >> 8] += (opcode & 0x00FF);
-        break;
-
-    case 0x8000:
-        switch (opcode & 0x000F) {
-        case 0x0000:
-            V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
-            break;
-
-        case 0x0001:
-            V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
-
-        case 0x0002:
-            V[(opcode & 0x0F00) >> 8] &= V[(opcode & 0x00F0) >> 4];
-            break;
-
-        case 0x0003:
-            V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];
-            break;
-
-        case 0x0004: {
-            unsigned short result = V[(opcode & 0x0F00) >> 8] + V[(opcode & 0x00F0) >> 4];
-            V[0xF] = result > 0xFF;
-            V[(opcode & 0x0F00) >> 8] = static_cast<unsigned char>(result & 0xFF);
-            break;
-        }
-        case 0x0005: {
-            V[0xF] = V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4];
-            unsigned short result = V[(opcode & 0x0F00) >> 8] - V[(opcode & 0x00F0) >> 4];
-            V[(opcode & 0x0F00) >> 8] = static_cast<unsigned char>(result & 0xFF);
-            break;
-        }
-        case 0x0006:
-            V[0xF] = (V[(opcode & 0x0F00) >> 8] & 0b0000001) == 0b0000001;
-            V[(opcode & 0x0F00) >> 8] >>= 1;
-            break;
-
-        case 0x000E:
-            V[0xF] = (V[(opcode & 0x0F00) >> 8] & 0b1000000) == 0b1000000;
-            V[(opcode & 0x0F00) >> 8] <<= 1;
-            break;
-
-        default:
-            printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
-            break;
-        }
-        break;
-
-    case 0x9000:
-        if (V[(opcode & 0x0F00 >> 8)] != V[(opcode & 0x00F0 >> 4)])
-            pc += 2;
-        break;
-
-    case 0xA000:
-        I = opcode & 0x0FFF;
-        break;
-    case 0xC000: {
-        V[(opcode & 0x0F00) >> 8] = rand() & (opcode & 0x00FF);
-        break;
-    }
-    case 0xD000: {
-        unsigned char x = V[(opcode & 0x0F00) >> 8] + 8;
-        unsigned char height = opcode & 0x000F;
-        unsigned char y = V[(opcode & 0x00F0) >> 4];
-        V[0xF] = false;
-
-        for (unsigned char row = 0; row < height; row++) {
-            unsigned char byte = memory[I + row];
-            for (char i = 0; i < 8; i++) {
-                unsigned short& pixel = gfx[((y + row) % 32) * 64 + (x - i - 1) % 64];
-                bool flip = byte & (1 << i);
-                if (pixel && flip)
-                    V[0xF] = true;
-                if (flip)
-                    pixel = ~pixel;
-            }
-        }
-
-        render->drawGraphics(gfx);
-        break;
-    }
-    case 0xE000:
-        switch (opcode & 0x00FF) {
-        case 0x009E:
-            if (keypad->keyIsPressed((opcode & 0x0F00) >> 8))
-                pc += 2;
-            break;
-
-        case 0x00A1:
-            if (!keypad->keyIsPressed((opcode & 0x0F00) >> 8))
-                pc += 2;
-            break;
-        }
-        break;
-
-    case 0xF000:
-        switch (opcode & 0x00FF) {
-        case 0x0007:
-            V[(opcode & 0x0F00) >> 8] = delay_timer;
-            break;
-
-        case 0x000A:
-            V[(opcode & 0x0F00) >> 8] = keypad->waitForInput();
-            break;
-
-        case 0x0015:
-            delay_timer = V[(opcode & 0x0F00) >> 8];
-            break;
-
-        case 0x0018:
-            sound_timer = V[(opcode & 0x0F00) >> 8];
-            break;
-
-        case 0x001E:
-            I += V[(opcode & 0x0F00) >> 8];
-            break;
-
-        case 0x0029:
-            I = V[(opcode & 0x0F00) >> 8] * 5;
-            break;
-
-        case 0x0033: {
-            unsigned char num = V[(opcode & 0x0F00) >> 8];
-            unsigned char cent = num / 100;
-            num %= 100;
-            unsigned char dec = num / 10;
-            num %= 10;
-            unsigned char unit = num;
-            memory[I] = cent;
-            memory[I + 1] = dec;
-            memory[I + 2] = unit;
-            break;
-        }
-        case 0x0055:
-            std::copy_n(std::execution::par_unseq, V.begin(), ((opcode & 0x0F00) >> 8) + 1,
-                        memory.begin() + I);
-            break;
-
-        case 0x0065:
-            std::copy_n(std::execution::par_unseq, memory.begin() + I, ((opcode & 0x0F00) >> 8) + 1,
-                        V.begin());
-            break;
-
-        default:
-            printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
-            break;
-        }
-        break;
-
-    default:
-        printf("Unknown opcode: 0x%X\n", opcode);
-        break;
-    }*/
+    CPU::opcode_table[op()](*cpu);
 
     if (delay_timer > 0)
         --delay_timer;
@@ -301,7 +105,7 @@ void Chip8::CPU::split_0() {
 }
 
 void Chip8::CPU::CLS() {
-    std::fill(sys->gfx.begin(), sys->gfx.end(), 0);
+    std::fill(std::execution::par_unseq, sys->gfx.begin(), sys->gfx.end(), 0);
     sys->pc += 2;
 }
 
@@ -424,11 +228,11 @@ void Chip8::CPU::DRW_Vx_Vy_nibble() {
         unsigned char byte = sys->memory[sys->I + row];
         for (char i = 0; i < 8; i++) {
             unsigned short& pixel = sys->gfx[((y + row) % 32) * 64 + (x - i - 1) % 64];
-            bool flip = byte & (1 << i);
-            if (pixel && flip)
-                sys->V[0xF] = true;
-            if (flip)
+            if (byte & (1 << i)) {
+                if (pixel)
+                    sys->V[0xF] = true;
                 pixel = ~pixel;
+            }
         }
     }
 
