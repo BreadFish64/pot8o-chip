1014e404ca18c32b6ab34dfca6f8fb16b85fe7be
BreadFish64 <mohror64@gmail.com>
2019-12-26 19:43:38 -0600

    redo key bindings

--- pot8o-chip/frontend.hpp
+++ pot8o-chip/frontend.hpp
@@ -39,9 +39,9 @@ private:
 
     // todo: add configuration somehow
     std::map<SDL_Keycode, std::size_t> key_map{
-        {SDLK_KP_0, 0x0},      {SDLK_KP_7, 0x1},        {SDLK_KP_8, 0x2},     {SDLK_KP_9, 0x3},
-        {SDLK_KP_4, 0x4},      {SDLK_KP_5, 0x5},        {SDLK_KP_6, 0x6},     {SDLK_KP_1, 0x7},
-        {SDLK_KP_2, 0x8},      {SDLK_KP_3, 0x9},        {SDLK_RIGHT, 0xA},    {SDLK_KP_PERIOD, 0xB},
+        {SDLK_KP_0, 0x0},      {SDLK_KP_1, 0x1},        {SDLK_KP_2, 0x2},     {SDLK_KP_3, 0x3},
+        {SDLK_KP_4, 0x4},      {SDLK_KP_5, 0x5},        {SDLK_KP_6, 0x6},     {SDLK_KP_7, 0x7},
+        {SDLK_KP_8, 0x8},      {SDLK_KP_9, 0x9},        {SDLK_RIGHT, 0xA},    {SDLK_KP_PERIOD, 0xB},
         {SDLK_KP_DIVIDE, 0xC}, {SDLK_KP_MULTIPLY, 0xD}, {SDLK_KP_MINUS, 0xE}, {SDLK_KP_PLUS, 0xF},
     };
 
